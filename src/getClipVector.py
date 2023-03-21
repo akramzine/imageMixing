386cdacd18ac8cb8a35edd2a3721f796e0b14065
akramzine <116067464+akramzine@users.noreply.github.com>
2023-03-21 17:21:45 +0100

    Update getClipVector.py

--- src/getClipVector.py
+++ src/getClipVector.py
@@ -1,16 +1,13 @@
-
 from latentdiffusion.ldm.models.diffusion.ddim import DDIMSampler
 from latentdiffusion.ldm.extras import load_model_from_config, load_training_dir
 
 import torch
 import clip.clip as clip
 from PIL import Image
-# rest of the code here
 import requests
 import base64
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-# Load the CLIP clip_model
 clip_model, preprocess = clip.load("ViT-L/14", device=device)
 clip_model.eval()
 ckpt="v1-5-pruned-emaonly.ckpt"
@@ -28,7 +25,7 @@ def get_clip_vector(image_path):
         clip_vector = clip_model.encode_image(tensor).float()
     return clip_vector
 
-def generate_image(clip_vector, scale=1.0, steps=30):
+def generate_image(clip_vector, scale=1.0, steps=50):  # Increased steps to 50
     with torch.no_grad():
         shape = [4, 512 // 8, 512 // 8]
         start_code = torch.randn(1, *shape, device=device)
@@ -42,16 +39,14 @@ def generate_image(clip_vector, scale=1.0, steps=30):
         generated_image = model.decode_first_stage(samples)[0]
     return generated_image
 
-# save the image to a file
-clip_vector=get_clip_vector("/home/ubuntu/images/oussamaammar/00001.jpg")
-generated_image=generate_image(clip_vector, scale=1.0, steps=30)
+clip_vector = get_clip_vector("/home/ubuntu/images/oussamaammar/00001.jpg")
+generated_image = generate_image(clip_vector, scale=1.0, steps=50)
 generated_image = generated_image.to('cpu').numpy()
-#generated_image = generated_image.transpose((1, 2, 0)) * 255.0
+generated_image = (generated_image.transpose((1, 2, 0)) * 255.0).clip(0, 255)  # Added clipping to [0, 255] range
 generated_image = generated_image.astype('uint8')
 image_pil = Image.fromarray(generated_image)
 image_pil.save("generated_image.jpg")
 
-# upload the image to ImgBB and get the URL
 with open("generated_image.jpg", "rb") as file:
     url = "https://api.imgbb.com/1/upload"
     payload = {
@@ -60,5 +55,3 @@ with open("generated_image.jpg", "rb") as file:
     }
     res = requests.post(url, payload)
     print(res.json())
-
-
