5c51bfec10b536acddc299c04d1ff3b86ac52bdb
akramzine <116067464+akramzine@users.noreply.github.com>
2023-03-21 14:36:43 +0100

    Update getClipVector.py

--- src/getClipVector.py
+++ src/getClipVector.py
@@ -42,7 +42,10 @@ def generate_image(clip_vector, scale=1.0, steps=30):
                                          verbose=False,
                                          unconditional_guidance_scale=scale,
                                          x_T=start_code)
-        generated_image = ddim_sampler.decode_first_stage(samples)[0]
+        # decode the samples using the model object
+        decoded_samples = model.decode(samples, timesteps=steps)
+        # get the last step of the decoded samples
+        generated_image = decoded_samples[-1][0]
     return generated_image
 print(generate_image(get_clip_vector("/home/ubuntu/images/oussamaammar/00001.jpg"),scale=1.0, steps=30))
 
