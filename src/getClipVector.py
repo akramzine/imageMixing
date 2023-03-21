e0086a391ceb222b233366cfaa3a5beed984ec11
akramzine <116067464+akramzine@users.noreply.github.com>
2023-03-21 14:59:56 +0100

    Update getClipVector.py

--- src/getClipVector.py
+++ src/getClipVector.py
@@ -21,11 +21,9 @@ ddim_sampler = DDIMSampler(model)
 
 def get_clip_vector(image_path):
     image = Image.open(image_path).convert("RGB")
-    # resize image
     tensor = preprocess(image).to(device)
     tensor = torch.unsqueeze(tensor, 0)
 
-    print(tensor.shape)
     with torch.no_grad():
         clip_vector = clip_model.encode_image(tensor).float()
     return clip_vector
@@ -33,7 +31,6 @@ def get_clip_vector(image_path):
 def generate_image(clip_vector, scale=1.0, steps=30):
     with torch.no_grad():
         shape = [4, 512 // 8, 512 // 8]
-        print(clip_vector.shape)
         start_code = torch.randn(1, *shape, device=device)
         samples, _ = ddim_sampler.sample(S=steps,
                                          conditioning=clip_vector.unsqueeze(0),
@@ -44,6 +41,24 @@ def generate_image(clip_vector, scale=1.0, steps=30):
                                          x_T=start_code)
         generated_image = model.decode_first_stage(samples)[0]
     return generated_image
-print(generate_image(get_clip_vector("/home/ubuntu/images/oussamaammar/00001.jpg"),scale=1.0, steps=30))
+generated_image = generate_image(clip_vector, scale=1.0, steps=30)
+
+# save the image to a file
+generated_image=generate_image(get_clip_vector("/home/ubuntu/images/oussamaammar/00001.jpg"),scale=1.0, steps=30)
+generated_image = generated_image.to('cpu').numpy()
+generated_image = generated_image.transpose((1, 2, 0)) * 255.0
+generated_image = generated_image.astype('uint8')
+image_pil = Image.fromarray(generated_image)
+image_pil.save("generated_image.jpg")
+
+# upload the image to ImgBB and get the URL
+with open("generated_image.jpg", "rb") as file:
+    url = "https://api.imgbb.com/1/upload"
+    payload = {
+        "key": "your_api_key",
+        "image": base64.b64encode(file.read()),
+    }
+    res = requests.post(url, payload)
+    print(res.json()['data']['url'])
 
 
