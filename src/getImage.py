e8172924c8c5e4420f07ef0a9c86a3f9b89fa751
akramzine <116067464+akramzine@users.noreply.github.com>
2023-03-22 14:31:31 +0100

    Update getImage.py

--- src/getImage.py
+++ src/getImage.py
@@ -14,10 +14,10 @@ from pytorch_lightning import seed_everything
 from torch import autocast
 from contextlib import contextmanager, nullcontext
 import base64
-from ldm.util import instantiate_from_config
-from ldm.models.diffusion.ddim import DDIMSampler
-from ldm.models.diffusion.plms import PLMSSampler
-from ldm.models.diffusion.dpm_solver import DPMSolverSampler
+from latentdiffusion.ldm.util import instantiate_from_config
+from latentdiffusion.ldm.models.diffusion.ddim import DDIMSampler
+from latentdiffusion.ldm.models.diffusion.plms import PLMSSampler
+from latentdiffusion.ldm.models.diffusion.dpm_solver import DPMSolverSampler
 import requests
 from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
 from transformers import AutoFeatureExtractor
