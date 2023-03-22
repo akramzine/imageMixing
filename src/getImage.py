b6dc8d4e11f9112987f037732df19265cdf08d4b
akramzine <116067464+akramzine@users.noreply.github.com>
2023-03-22 16:52:58 +0100

    Update getImage.py

--- src/getImage.py
+++ src/getImage.py
@@ -300,6 +300,10 @@ def main():
                             prompts = list(prompts)
                         c=c.repeat(1, uc.shape[1], 1)
                         c = c.repeat(1, 1, 3)
+                        c = torch.einsum('ik,jk->ij', clip_vector, token_embeddings)
+                        c = c / torch.norm(clip_vector, dim=-1)[:, None]
+                        c = c / torch.norm(token_embeddings, dim=-1).unsqueeze(1)
+
                         print(uc.size())
                         print(c.size())
                         shape = [opt.C, opt.H // opt.f, opt.W // opt.f]
