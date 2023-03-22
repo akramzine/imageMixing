549295d6dbd53d55b8c96cdf4f93520dc86dcb28
akramzine <116067464+akramzine@users.noreply.github.com>
2023-03-22 14:19:41 +0100

    Update getImage.py

--- src/getImage.py
+++ src/getImage.py
@@ -13,16 +13,18 @@ import time
 from pytorch_lightning import seed_everything
 from torch import autocast
 from contextlib import contextmanager, nullcontext
-
+import base64
 from ldm.util import instantiate_from_config
 from ldm.models.diffusion.ddim import DDIMSampler
 from ldm.models.diffusion.plms import PLMSSampler
 from ldm.models.diffusion.dpm_solver import DPMSolverSampler
-
+import requests
 from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
 from transformers import AutoFeatureExtractor
 
-
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+clip_model, preprocess = clip.load("ViT-L/14", device=device)
+clip_model.eval()
 # load safety model
 safety_model_id = "CompVis/stable-diffusion-safety-checker"
 safety_feature_extractor = AutoFeatureExtractor.from_pretrained(safety_model_id)
@@ -94,17 +96,18 @@ def check_safety(x_image):
             x_checked_image[i] = load_replacement(x_checked_image[i])
     return x_checked_image, has_nsfw_concept
 
+def get_clip_vector(image_path):
+    image = Image.open(image_path).convert("RGB")
+    tensor = preprocess(image).to(device)
+    tensor = torch.unsqueeze(tensor, 0)
+
+    with torch.no_grad():
+        clip_vector = clip_model.encode_image(tensor).float()
+    return clip_vector
 
 def main():
     parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        "--prompt",
-        type=str,
-        nargs="?",
-        default="a painting of a virus monster playing guitar",
-        help="the prompt to render"
-    )
+    
     parser.add_argument(
         "--outdir",
         type=str,
@@ -239,7 +242,7 @@ def main():
         opt.config = "configs/latent-diffusion/txt2img-1p4B-eval.yaml"
         opt.ckpt = "models/ldm/text2img-large/model.ckpt"
         opt.outdir = "outputs/txt2img-samples-laion400m"
-
+ 
     seed_everything(opt.seed)
 
     config = OmegaConf.load(f"{opt.config}")
@@ -247,7 +250,7 @@ def main():
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model = model.to(device)
-
+    
     if opt.dpm_solver:
         sampler = DPMSolverSampler(model)
     elif opt.plms:
@@ -265,16 +268,9 @@ def main():
 
     batch_size = opt.n_samples
     n_rows = opt.n_rows if opt.n_rows > 0 else batch_size
-    if not opt.from_file:
-        prompt = opt.prompt
-        assert prompt is not None
-        data = [batch_size * [prompt]]
-
-    else:
-        print(f"reading prompts from {opt.from_file}")
-        with open(opt.from_file, "r") as f:
-            data = f.read().splitlines()
-            data = list(chunk(data, batch_size))
+    clip_vector = get_clip_vector("/home/ubuntu/images/oussamaammar/00001.jpg")
+    clip_vector = clip_vector.to(device)  # Move the clip vector to the device (CPU or GPU)
+    data = [clip_vector for _ in range(batch_size)]  # Repeat the clip vector for the batch size
 
     sample_path = os.path.join(outpath, "samples")
     os.makedirs(sample_path, exist_ok=True)
@@ -298,7 +294,7 @@ def main():
                             uc = model.get_learned_conditioning(batch_size * [""])
                         if isinstance(prompts, tuple):
                             prompts = list(prompts)
-                        c = model.get_learned_conditioning(prompts)
+                        c = clip_vector.unsqueeze(0).repeat(batch_size, 1, 1, 1)  # Repeat the clip vector for the batch size and add a batch dimension
                         shape = [opt.C, opt.H // opt.f, opt.W // opt.f]
                         samples_ddim, _ = sampler.sample(S=opt.ddim_steps,
                                                          conditioning=c,
@@ -323,25 +319,17 @@ def main():
                                 x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
                                 img = Image.fromarray(x_sample.astype(np.uint8))
                                 img = put_watermark(img, wm_encoder)
-                                img.save(os.path.join(sample_path, f"{base_count:05}.png"))
+                                img.save("image"+str(base_count)+".jpg")
+                                with open("image"+str(base_count)+".jpg", "rb") as file:
+                                    url = "https://api.imgbb.com/1/upload"
+                                    payload = {
+                                        "key": "a06c97753bd86796d1159e4ca7f1efc2",
+                                        "image": base64.b64encode(file.read()),
+                                    }
+                                    res = requests.post(url, payload)
+                                    print(res.json())
                                 base_count += 1
-
-                        if not opt.skip_grid:
-                            all_samples.append(x_checked_image_torch)
-
-                if not opt.skip_grid:
-                    # additionally, save as grid
-                    grid = torch.stack(all_samples, 0)
-                    grid = rearrange(grid, 'n b c h w -> (n b) c h w')
-                    grid = make_grid(grid, nrow=n_rows)
-
-                    # to image
-                    grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
-                    img = Image.fromarray(grid.astype(np.uint8))
-                    img = put_watermark(img, wm_encoder)
-                    img.save(os.path.join(outpath, f'grid-{grid_count:04}.png'))
-                    grid_count += 1
-
+                                
                 toc = time.time()
 
     print(f"Your samples are ready and waiting for you here: \n{outpath} \n"
