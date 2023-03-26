c821451ac1da04dce67c72bad1fc997a55c1e9df
akramzine <116067464+akramzine@users.noreply.github.com>
2023-03-26 21:07:01 +0200

    Update txt2img.py

--- src/txt2img.py
+++ src/txt2img.py
@@ -1,8 +1,5 @@
-import requests
-import base64
 from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 import torch
-
 model_id = "stabilityai/stable-diffusion-2-1"
 
 # Use the DPMSolverMultistepScheduler (DPM-Solver++) scheduler here instead
@@ -12,18 +9,5 @@ pipe = pipe.to("cuda")
 
 prompt = "a photo of an astronaut riding a horse on mars"
 image = pipe(prompt).images[0]
-
-# Encode the image data in base64 format
-image_data = base64.b64encode(image.tobytes()).decode("utf-8")
-
-# Upload the image to imgbb
-api_key = "your_api_key"
-response = requests.post("https://api.imgbb.com/1/upload", 
-                         data={"key": api_key, "image": image_data})
-
-# Print the response
-if response.status_code == 200:
-    image_url = response.json()["data"]["url"]
-    print("Image URL: ", image_url)
-else:
-    print("Error: ", response.text)
+    
+image.save("astronaut_rides_horse.png")
