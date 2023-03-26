72f51e326573ca69ea06e814176f295bcded0b58
akramzine <116067464+akramzine@users.noreply.github.com>
2023-03-26 21:12:59 +0200

    Update txt2img.py

--- src/txt2img.py
+++ src/txt2img.py
@@ -1,10 +1,10 @@
-from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+from diffusers import StableDiffusionPipeline, EulerAncestralDiscreteScheduler
 import torch
 model_id = "stabilityai/stable-diffusion-2-1"
 
 # Use the DPMSolverMultistepScheduler (DPM-Solver++) scheduler here instead
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
-pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("cuda")
 
 prompt = "a photo of an astronaut riding a horse on mars"
