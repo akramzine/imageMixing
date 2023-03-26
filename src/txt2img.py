ccf94d3e832b2822c99eebe7d708d151b8ee500a
akramzine <116067464+akramzine@users.noreply.github.com>
2023-03-26 21:17:58 +0200

    Update txt2img.py

--- src/txt2img.py
+++ src/txt2img.py
@@ -10,4 +10,4 @@ pipe = pipe.to("cuda")
 prompt = "a close up of a woman's face with stars in the background, by Yoshio Markino, tumblr, the little mermaid, kiss, animated movie still, sleeping beauty AND a woman standing in the water holding a stick, a photo, by Doug Wildey, flickr, magic realism, tuesday weld in a pink bikini, film still dnd movie, on an island, motu"
 image = pipe(prompt).images[0]
     
-image.save("a close up of a woman's face with stars in the background, by Yoshio Markino, tumblr, the little mermaid, kiss, animated movie still, sleeping beauty AND a woman standing in the water holding a stick, a photo, by Doug Wildey, flickr, magic realism, tuesday weld in a pink bikini, film still dnd movie, on an island, motu.png")
+image.save("photooos.png")
